4bde635747c36b645f15a1abcdd0385502cfc94d
Nobuhiro Miyamot <verlorenesiege@gmail.com>
2025-07-12 14:28:40 +0900

    API 追加

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
+import {ref, onMounted } from "vue"
+import axios from 'axios'
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+
+const text = ref("")
+
+  onMounted(async () => {
+     const response = await axios.get("/api/subscription?name=Miya")
+     console.log(response.data)
+     text.value = response.data
+  })
 </script>
 
 <template>
@@ -8,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld :msg="text" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
